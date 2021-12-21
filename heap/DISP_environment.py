648a8dee3df8fbce6c783c72bb7baa8a8c1e9624
Florian Berlinger <fberlinger@seas.harvard.edu>
2021-12-21 11:19:06 +0100

    started with disperion

---
+++ heap/DISP_environment.py
@@ -0,0 +1,310 @@
+"""Central data base keeping track of positions, velocities, relative positions, and distances of all simulated fishes
+"""
+import math
+import random
+import numpy as np
+from scipy.spatial.distance import cdist
+import sys
+U_LED_DX = 86 # [mm] leds x-distance on BlueBot
+U_LED_DZ = 86 # [mm] leds z-distance on BlueBot
+
+class Environment():
+    """Simulated fish environment
+    
+    Fish get their visible neighbors and corresponding relative positions and distances from here. Fish also update their own positions after moving in here. Environmental tracking data is used for simulation analysis.
+    """
+
+    def __init__(self, pos, vel, fish_specs, arena):
+        # Arguments
+        self.pos = pos # x, y, z, phi; [no_robots X 4]
+        self.vel = vel # pos_dot
+        self.v_range = fish_specs[0] # visual range, [mm]
+        self.w_blindspot = fish_specs[1] # width of blindspot, [mm]
+        self.r_sphere = fish_specs[2] # radius of blocking sphere for occlusion, [mm]
+        self.n_magnitude = fish_specs[3] # visual noise magnitude, [% of distance]
+        self.arena_size = arena # x, y, z
+        
+        # Parameters
+        self.no_robots = self.pos.shape[0]
+        self.no_states = self.pos.shape[1]
+
+        # Initialize robot states
+        self.init_states()
+
+        # Initialize tracking
+        self.init_tracking()
+
+        # Initialize LEDs
+        #self.leds_pos = [np.zeros((3,3))]*self.no_robots # empty init, filled with update_leds() below
+        #for robot in range(self.no_robots):
+        #    self.update_leds(robot)
+
+    def log_to_file(self, filename):
+        """Logs tracking data to file
+        """
+        #np.savetxt('./logfiles/{}_data.txt'.format(filename), self.tracking, fmt='%.2f', delimiter=',')
+        np.savetxt('./logfiles/{}.txt'.format(filename), self.tracking, fmt='%.2f', delimiter=',')
+
+    def init_tracking(self):
+        """Initializes tracking
+        """
+        pos = np.reshape(self.pos, (1,self.no_robots*self.no_states))
+        vel = np.reshape(self.vel, (1,self.no_robots*self.no_states))
+        self.tracking = np.concatenate((pos,vel), axis=1)
+        self.updates = 0
+
+    def update_tracking(self):
+        """Updates tracking after every fish took a turn
+        """
+        pos = np.reshape(self.pos, (1,self.no_robots*self.no_states))
+        vel = np.reshape(self.vel, (1,self.no_robots*self.no_states))
+        current_state = np.concatenate((pos,vel), axis=1)
+        self.tracking = np.concatenate((self.tracking,current_state), axis=0)
+    
+    def update_leds(self, source_index):
+        """ Updates the position of the three leds based on self.pos, which is the position of led1
+        """
+        pos = self.pos[source_index,:3]
+        phi = self.pos[source_index,3]
+
+        x1 = pos[0]
+        x2 = x1
+        x3 = x1 + math.cos(phi)*U_LED_DX
+
+        y1 = pos[1]
+        y2 = y1
+        y3 = y1 + math.sin(phi)*U_LED_DX
+
+        z1 = pos[2]
+        z2 = z1 + U_LED_DZ
+        z3 = z1
+
+        self.leds_pos[source_index] = np.array([[x1, x2, x3],[y1, y2, y3],[z1, z2, z3]])
+
+    def init_states(self):
+        """Initializes fish positions and velocities
+        """
+        # Restrict initial positions to arena size
+        self.pos[:,0] = np.clip(self.pos[:,0], 0, self.arena_size[0])
+        self.pos[:,1] = np.clip(self.pos[:,1], 0, self.arena_size[1])
+        self.pos[:,2] = np.clip(self.pos[:,2], 0, self.arena_size[2])
+
+        # Initial relative positions
+        a_ = np.reshape(self.pos, (1, self.no_robots*self.no_states))
+        a = np.tile(a_, (self.no_robots,1))
+        b = np.tile(self.pos, (1,self.no_robots))
+        self.rel_pos = a - b # [4*no_robots X no_robots]
+
+        # Initial distances
+        self.dist = cdist(self.pos[:,:3], self.pos[:,:3], 'euclidean') # without phi; [no_robots X no_robots]
+
+    def update_states(self, source_id, pos, vel): # add noise
+        """Updates a fish state and affected realtive positions and distances
+        """
+        # Position and velocity
+        self.pos[source_id,0] = np.clip(pos[0], 0, self.arena_size[0])
+        self.pos[source_id,1] = np.clip(pos[1], 0, self.arena_size[1])
+        self.pos[source_id,2] = np.clip(pos[2], 0, self.arena_size[2])
+        self.pos[source_id,3] = pos[3]
+        self.vel[source_id,:] = vel
+
+        # Relative positions
+        pos_others = np.reshape(self.pos, (1,self.no_robots*self.no_states))
+        pos_self = np.tile(self.pos[source_id,:], (1,self.no_robots))
+        rel_pos = pos_others - pos_self
+        self.rel_pos[source_id,:] = rel_pos # row
+        rel_pos_ = np.reshape(rel_pos, (self.no_robots, self.no_states))
+        self.rel_pos[:,source_id*self.no_states:source_id*self.no_states+self.no_states] = -rel_pos_ # columns
+        
+        # Relative distances
+        dist = np.linalg.norm(rel_pos_[:,:3], axis=1) # without phi
+        self.dist[source_id,:] = dist
+        self.dist[:,source_id] = dist.T
+
+        # Update LEDs
+        #self.update_leds(source_id)
+
+        # Update tracking
+        self.updates += 1
+        if self.updates >= self.no_robots:
+            self.updates = 0
+            self.update_tracking()
+
+    def get_robots(self, source_id, visual_noise=False):
+        """Provides visible neighbors and relative positions and distances to a fish
+        """
+        robots = set(range(self.no_robots)) # all robots
+        robots.discard(source_id) # discard self
+
+        rel_pos = np.reshape(self.rel_pos[source_id], (self.no_robots, self.no_states))
+
+        return (robots, rel_pos, self.dist[source_id])
+
+        # perfect vision here
+        '''
+        self.visual_range(source_id, robots)
+        self.blind_spot(source_id, robots, rel_pos)
+        self.occlusions(source_id, robots, rel_pos)
+
+        leds = self.calc_relative_leds(source_id, robots)
+
+        if self.n_magnitude: # no overwrites of self.rel_pos and self.dist
+            n_rel_pos, n_dist = self.visual_noise(source_id, rel_pos)
+            return (robots, n_rel_pos, n_dist, leds)
+        return (robots, rel_pos, self.dist[source_id], leds)
+        '''
+
+    def visual_range(self, source_id, robots):
+        """Deletes fishes outside of visible range
+        """
+        conn_drop = 0.005
+        
+        candidates = robots.copy()
+        for robot in candidates:
+            d_robot = self.dist[source_id][robot]
+            x = conn_drop * (d_robot - self.v_range)
+            if x < -5:
+                sigmoid = 1
+            elif x > 5:
+                sigmoid = 0
+            else:
+                sigmoid = 1 / (1 + math.exp(x))
+            prob = random.random()
+
+            if  sigmoid < prob:
+                robots.remove(robot)
+
+    def blind_spot(self, source_id, robots, rel_pos):
+        """Omits fishes within the blind spot behind own body
+        """
+        r_blockage = self.w_blindspot/2
+
+        phi = self.pos[source_id,3]
+        phi_xy = [math.cos(phi), math.sin(phi)]
+        mag_phi = np.linalg.norm(phi_xy)
+        
+        candidates = robots.copy()
+        for robot in candidates:
+            dot = np.dot(phi_xy, rel_pos[robot,:2])
+            if dot < 0:
+                d_robot = np.linalg.norm(rel_pos[robot,:2])
+
+                angle = abs(math.acos(dot / (mag_phi * d_robot))) - math.pi / 2 # cos(a-b) = ca*cb+sa*sb = sa
+
+                if  math.cos(angle) * d_robot < r_blockage:
+                    robots.remove(robot)
+
+    def occlusions(self, source_id, robots, rel_pos):
+        """Omits invisible fishes occluded by others
+        """
+
+
+        rel_dist = self.dist[source_id]
+        id_by_dist = np.argsort(rel_dist)
+
+        n_valid = []
+        for robot in id_by_dist[1:]:
+            if not robot in robots:
+                continue
+            occluded = False
+            d_robot = rel_dist[robot]
+            if d_robot == 0: # "collision"
+                continue
+            coord_robot = rel_pos[robot,:3]
+
+            for verified in n_valid:
+                d_verified = rel_dist[verified]
+                coord_verified = rel_pos[verified,:3]
+
+                theta_min = math.asin(self.r_sphere / d_verified)
+                theta = abs(math.acos(np.dot(coord_robot, coord_verified) / (d_robot * d_verified)))
+
+                if theta < theta_min:
+                    occluded = True
+                    robots.remove(robot)
+                    if not robots:
+                        return
+                    break
+
+            if not occluded:
+                n_valid.append(robot)
+
+    def visual_noise(self, source_id, rel_pos):
+        """Adds visual noise
+        """
+        magnitudes = self.n_magnitude * np.array([self.dist[source_id]]).T
+        noise = magnitudes * (np.random.rand(self.no_robots, self.no_states) - 0.5) # zero-mean uniform noise
+        n_rel_pos = rel_pos + noise
+        n_dist = np.linalg.norm(n_rel_pos[:,:3], axis=1) # new dist without phi
+
+        return (n_rel_pos, n_dist)
+
+    def see_circlers(self, source_id, robots, rel_pos, sensing_angle):
+        '''For circle formation
+        '''
+        phi = self.pos[source_id,3]
+        phi_xy = [math.cos(phi), math.sin(phi)]
+        mag_phi = np.linalg.norm(phi_xy)
+
+        candidates = robots.copy()
+        for robot in candidates:
+            dot = np.dot(phi_xy, rel_pos[robot,:2])
+            if dot > 0:
+                d_robot = np.linalg.norm(rel_pos[robot,:2])
+
+                angle = abs(math.acos(dot / (mag_phi * d_robot)))
+
+                if (angle*180/math.pi) < (sensing_angle/2):
+                    return True
+
+        return False
+
+    def rot_global_to_robot(self, phi):
+        """Rotate global coordinates to robot coordinates. Used before simulation of dynamics.
+        """
+        return np.array([[math.cos(phi), math.sin(phi), 0], [-math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
+
+    def rot_robot_to_global(self, phi):
+        """Rotate robot coordinates to global coordinates. Used after simulation of dynamics.
+        """
+        return np.array([[math.cos(phi), -math.sin(phi), 0], [math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
+
+    def calc_reflections(self, leds_list):
+        """Calculates the position of the reflected leds
+        """
+        refl_list = []
+        for led in leds_list:
+            if led[2] > 10: # at least 10 mm below surface to have a reflection
+                refl = led + np.array([0,0, -2*led[2]])
+                refl_list.append(refl)
+        return refl_list
+
+    def calc_relative_leds(self, source_id, robots):
+        """Calculates the relative position of all detectable leds and adds their reflection if add_reflections boolean is set to True
+        """
+        if not robots:
+            return np.empty((3,0))
+
+        add_reflections = True
+        all_blobs = np.empty((3,0))
+        
+        leds = []
+        for robot in robots:
+            leds.append(self.leds_pos[robot])
+
+        leds_list = list(np.transpose(np.hstack(leds)))
+        if add_reflections:
+            refl_list = self.calc_reflections(leds_list)
+            leds_list = leds_list + refl_list
+
+        my_pos = self.pos[source_id,:3]
+        my_phi = self.pos[source_id,3]
+        R = self.rot_global_to_robot(my_phi)
+
+        for led in leds_list:
+            relative_coordinates = R @ ((led - my_pos)[:, np.newaxis])
+            relative_coordinates /= np.linalg.norm(relative_coordinates) # normalize from xyz to pqr
+            all_blobs = np.append(all_blobs, relative_coordinates, axis=1)
+
+        p = np.random.permutation(np.shape(all_blobs)[1]) # mix up into random order
+        return all_blobs[:,p]
---
+++ heap/fishfood/DISP_leader.py
@@ -0,0 +1,163 @@
+"""Simulates a Bluebot. Add behavioral code here.
+"""
+from math import *
+import numpy as np
+import random
+import time
+
+class Fish():
+    """Bluebot instance
+    """
+    
+    def __init__(self, my_id, target_dist, dynamics, environment):
+        # Arguments
+        self.id = my_id
+        self.dynamics = dynamics
+        self.environment = environment
+
+        # Bluebot features
+        self.body_length = 130
+
+        # Fins
+        self.caudal = 0
+        self.dorsal = 0
+        self.pect_r = 0
+        self.pect_l = 0
+
+        # Experimental parameter
+        self.r_target = target_dist * self.body_length
+
+
+    def run(self, duration):
+        """(1) Get neighbors from environment, (2) move accordingly, (3) update your state in environment
+        """
+        robots, rel_pos, dist = self.environment.get_robots(self.id)
+        target_pos, vel = self.move(robots, rel_pos, dist, duration)
+        self.environment.update_states(self.id, target_pos, vel)
+
+    def lj_force(self, robots, rel_pos, dist):
+        """lj_force derives the Lennard-Jones potential and force based on the relative positions of all neighbors and the desired self.target_dist to neighbors. The force is a gain factor, attracting or repelling a fish from a neighbor. The center is a point in space toward which the fish will move, based on the sum of all weighted neighbor positions.
+        """
+        a = 12
+        b = 6
+        epsilon = 100 # depth of potential well, V_LJ(r_target) = epsilon
+        gamma = 1 # force gain
+        r_const = 1.1*self.r_target# + 2 * self.body_length
+
+        center = np.zeros((3,))
+        n = len(robots)
+
+        for robot in robots:
+            r =min(dist[robot], r_const)
+            f_lj = -gamma*epsilon/r * (a*(self.r_target/r)**a - 2*b*(self.r_target/r)**b)
+            center += f_lj * rel_pos[robot,:3]
+
+        center /= n
+        magn = np.linalg.norm(center) # normalize
+        center /= magn # normalize
+
+        return (center, magn)
+
+    def depth_ctrl_vision(self, r_move_g):
+        """Vision-like depth control
+        
+        Args:
+            r_move_g (np.array): Relative position of desired goal location in robot frame.
+        """
+        pitch_range = 2 # abs(pitch) below which dorsal fin is not controlled
+
+        pitch = np.arctan2(r_move_g[2], sqrt(r_move_g[0]**2 + r_move_g[1]**2)) * 180 / pi
+
+        if pitch > pitch_range:
+            self.dorsal = 1
+        elif pitch < -pitch_range:
+            self.dorsal = 0
+
+    def depth_ctrl_psensor(self, r_move_g):
+        """Pressure-sensor-like depth control
+        
+        Args:
+            r_move_g (np.array): Relative position of desired goal location in robot frame.
+        """
+        depth = self.environment.pos[self.id,2]
+        target_depth = depth + r_move_g[2]
+
+        if depth > target_depth:
+            self.dorsal = 1
+        else:
+            self.dorsal = 0
+
+    def home(self, r_move_g, magnitude):
+        """Homing behavior. Sets fin controls to move toward a desired goal location.
+        
+        Args:
+            r_move_g (np.array): Relative position of desired goal location in robot frame.
+            magnitude (TYPE): Description
+        """
+        caudal_range = 35 # abs(heading) below which caudal fin is switched on
+        freq_c = min(1.5/4 + 1/1000 * magnitude, 0.5) # 0.5 should be some 2 Hz
+
+        # target lost or occluded
+        if not r_move_g.size:
+            self.pect_r = 0
+            self.pect_l = 0
+            self.caudal = 0
+            return
+
+        heading = np.arctan2(r_move_g[1], r_move_g[0]) * 180 / pi
+        # target behind
+        if heading > 155 or heading < -155:
+            self.caudal = 0
+            self.pect_r = 0.5
+            self.pect_l = 0.5
+
+        # target in front
+        elif heading < 10 and heading > -10:
+            self.pect_r = 0
+            self.pect_l = 0
+            self.caudal = freq_c
+
+        # target to the right
+        elif heading > 10:
+            freq_l = 0.2 + 0.3 * abs(heading) / 155
+            self.pect_l = freq_l
+            self.pect_r = 0
+
+            if heading < caudal_range:
+                self.caudal = freq_c
+            else:
+                self.caudal = 0
+
+    def move(self, robots, rel_pos, dist, duration):
+        """Decision-making based on neighboring robots and corresponding move
+        """
+        if not robots: # no robots, continue with ctrl from last step
+            target_pos, self_vel = self.dynamics.simulate_move(self.id, duration)
+            return (target_pos, self_vel)
+
+        # Define your move here
+        center, magnitude = self.lj_force(robots, rel_pos, dist)
+        move = center
+
+        '''
+        # Global to Robot Transformation
+        phi = self.environment.pos[self.id,3]
+        r_T_g = self.environment.rot_global_to_robot(phi)
+        r_move_g = r_T_g @ move
+
+        self.depth_ctrl_vision(r_move_g)
+        self.home(r_move_g, magnitude)
+
+        self.dynamics.update_ctrl(self.dorsal, self.caudal, self.pect_r, self.pect_l)
+
+        target_pos, self_vel = self.dynamics.simulate_move(self.id, duration)
+        '''
+
+        # POINT-MASS MOVE AT CONSTANT SPEED (0.5 BL PER ITERATION = 1 BL / S)
+        pos = self.environment.pos[self.id,:]
+        pos[:3] += move * self.body_length/10
+        pos[3] += np.arctan2(move[1], move[0])
+        target_pos = pos
+        self_vel = np.array([0, 0, 0 ,0])
+
+        return (target_pos, self_vel)
